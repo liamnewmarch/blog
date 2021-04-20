a907c22766f0ee041cdc60619c7f5219468cd6c1
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2021-04-20 18:29:39 +0100

    Add old static files, minify JS in production

--- static/rollup.config.js
+++ static/rollup.config.js
@@ -1,7 +1,10 @@
 import browsersync from 'rollup-plugin-browsersync';
 import css from 'rollup-plugin-css-only';
+import copy from 'rollup-plugin-copy';
+import { terser } from 'rollup-plugin-terser';
 
 const isWatch = Boolean(process.env.ROLLUP_WATCH);
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   input: 'src/index.js',
@@ -17,5 +20,17 @@ export default {
     css({
       output: 'index.css',
     }),
+    copy({
+      copyOnce: true,
+      flatten: false,
+      targets: [{
+        src: [
+          'src/*.png',
+          'src/*.txt',
+        ],
+        dest: 'build',
+      }],
+    }),
+    isProduction && terser(),
   ],
 };

93b6212ef545e84a1d2142a27961cea29e0840ce
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2026-04-20 20:11:36 +0100

    Remove Eleventy files and update project docs

--- static/index.js
+++
@@ -1,7 +0,0 @@
-customElements.define('current-year', class extends HTMLElement {
-  connectedCallback() {
-    this.append(' ', new Intl.DateTimeFormat('en', {
-      year: 'numeric',
-    }).format(), ' ')
-  }
-})

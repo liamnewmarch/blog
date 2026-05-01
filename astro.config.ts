06e9393a362dce54d70eac192a2cd1adc40e1880
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2026-05-01 13:49:06 +0100

    Emit assets instead of inlining, restructure for future components

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,35 @@
 import type { AstroUserConfig } from 'astro'
 
+const rollupOptions = {
+  output: {
+    assetFileNames: 'static/[hash][extname]',
+    chunkFileNames: 'static/[hash].js',
+    entryFileNames: 'static/[hash].js',
+  },
+}
+
 export default {
   site: 'https://liam.nwmr.ch',
   outDir: 'build',
+  build: {
+    assets: 'static',
+    inlineStylesheets: 'never',
+  },
   compressHTML: true,
   markdown: {
     syntaxHighlight: 'prism',
+  },
+  vite: {
+    build: {
+      assetsInlineLimit: 0, // prevent JS from being inlined
+      rollupOptions, // TODO remove when CSS uses environments...
+    },
+    environments: {
+      client: {
+        build: {
+          rollupOptions,
+        },
+      },
+    },
   },
 } satisfies AstroUserConfig
---
+++ src/static/js/index.ts
@@ -0,0 +1,1 @@
+import './components/current-year.ts'
